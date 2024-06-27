c72a7110514da4b74d91c778c8bf1a61f16fdc94
Dayveed021 <boskey021@gmail.com>
2024-06-27 18:10:13 -0500

    prepping vercel

--- Backend/users/urls.py
+++ Backend/users/urls.py
@@ -2,7 +2,7 @@ from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    re_path('', views.login),
+    re_path('login', views.login),
     re_path('signup', views.signup),
     re_path('test_token', views.test_token),
     re_path(r'^update/(?P<pk>\d+)$', views.update_user, name='update-user'),
