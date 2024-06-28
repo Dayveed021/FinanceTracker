ccb2726e9e12227185de16c36c13f44d91c10acb
Dayveed021 <boskey021@gmail.com>
2024-06-27 20:57:33 -0500

    backend hosting

--- Backend/financetracker/settings.py
+++ Backend/financetracker/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-quigrxya6*c%@)k#2np_ds(m-c36*@)eo8=1jjx8m&(iuzj9*p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = ['.vercel.app', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app','now.sh','localhost', '127.0.0.1']
 
 
 # Application definition
@@ -92,8 +93,12 @@ WSGI_APPLICATION = 'financetracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'TppoeBhncqBCgBGoXgTFAgXfuvpMFeRs',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '36351',
     }
 }
 
@@ -138,3 +143,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
